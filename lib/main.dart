c5e93148d46de054ae04a2e03cdac5b27decda4a
jeansprogrammer <jeansilva20042019@gmail.com>
2026-01-22 21:12:44 -0300

    Jean mudando a cor para verde

--- lib/main.dart
+++ lib/main.dart
@@ -113,6 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
